bb23fd69d19adc978a9ca86662e7074895f1645e
Geetha <geetha.ganabathula@hogarthww.com>
2015-11-18 17:29:34 +0000

    updtaed runner file to generate testreports

--- src/test/java/com/kids/ads/RunTest.java
+++ src/test/java/com/kids/ads/RunTest.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		format = {"pretty", 
-				"html:target/cucumber-report"},
+				"html:target/cucumber",
+				"json :target/cucumber.json"},
 		features = {"src/test/resources"}
 		)
 public class RunTest {
